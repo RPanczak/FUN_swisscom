88dcd57914053a88b05f463d2e112b25cb7b9c27
Radek <r.panczak@gmail.com>
2021-12-20 22:21:31 +0100

    Chansy example

--- python/api.py
+++ python/api.py
@@ -37,14 +37,26 @@ oauth.fetch_token(token_url = TOKEN_URL, client_id = client_id, client_secret = 
 
 
 # Get all the first MAX_NB_TILES_REQUEST tile ids associated with the postal code of interest
-municipality = 351
+# Chansy in SW corner of CH
+# Postal code(s)	1284
+# SFOS number	6611
+municipality = 6611
 muni_tiles_json = oauth.get(
     BASE_URL + "/grids/municipalities/{0}".format(municipality), headers = headers
 ).json()
+
+type(muni_tiles_json)
+len(muni_tiles_json["tiles"])
+
+muni_tiles_json.items()
+muni_tiles_json["tiles"][1]
+
 tile_ids = [t["tileId"] for t in muni_tiles_json["tiles"]][
     :MAX_NB_TILES_REQUEST
 ]
 
+type(tile_ids)
+len(tile_ids)
 
 # define a start time and request the density for the following given number of hours
 start_time = datetime(year=2020, month=1, day=27, hour=0, minute=0)
@@ -61,11 +73,10 @@ for dt in dates:
     daily_total_density = sum(
         map(
             lambda t: t["score"],
-            oauth.get(api_request, headers=headers).json()["tiles"],
+            oauth.get(api_request, headers = headers).json()["tiles"],
         )
     )
     date2score[dt.isoformat()] = daily_total_density
-
 
 # print the daily density for every date
 print("The average hourly density for postal code {0}".format(municipality))
